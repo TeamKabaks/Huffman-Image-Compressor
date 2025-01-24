9b77ff7337f9bf0fff3b896aa87844b1e86236ac
Norman Enrico C. Eulin <eulinnormanenricocatalan@gmail.com>
2025-01-24 16:30:46 +0800

    Update (unintegrated with Huffman Coding)

--- GUI.java
+++ GUI.java
@@ -1,80 +1,116 @@
-import java.awt.*;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import javax.swing.*;
 
-public class GUI {
+public class GUI{
+    private JPanel mainPanel, imagePanel, buttonPanel, buttonContainerPanel;
+    private JLabel imageLabel; 
+    private JButton newBtn, trainBtn, compressBtn, openBtn;
+    private ButtonOperations buttonOperations;
+    private JFrame mainFrame;
+    
+    public GUI(ButtonOperations buttonOperations){
+        this.buttonOperations =  buttonOperations;
+        this.imageLabel = buttonOperations.imageLabel;
+        this.newBtn = buttonOperations.getNewBtn();
+        this.trainBtn = buttonOperations.getTrainBtn();
+        this.compressBtn = buttonOperations.getCompressBtn();
+        this.openBtn = buttonOperations.getOpenBtn();
+    }
+
     public void createAndShowGUI(){
-      
-      JFrame frame = new JFrame("Image Compressor");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setSize(1200, 800);
-      frame.setLocationRelativeTo(null);
+        // (Used methods to organize)
+        createMainFrame();
+        createMainPanel();
+        createImagePanel();
+        createButtonPanel();
 
-      JPanel mainPanel = new JPanel(new BorderLayout());
+        GridBagConstraints gbcPanels = new GridBagConstraints();
+        gbcPanels.fill = GridBagConstraints.BOTH;
+        gbcPanels.weighty = 1.0;
 
-      JPanel viewerPanel = new JPanel();
-      viewerPanel.setPreferredSize(new Dimension(200, 200)); // Ensure it's square
-      viewerPanel.setBackground(Color.LIGHT_GRAY); // Example background color
+        gbcPanels.gridx = 0;
+        gbcPanels.weightx = 0.9; // 90% of the main panel width
+        mainPanel.add(imagePanel, gbcPanels);
 
-      JPanel buttonPanel = new JPanel();
-      buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-      buttonPanel.setBackground(Color.WHITE); // Example background color
-      buttonPanel.setPreferredSize(new Dimension(400, 200)); // Rectangular lengthwise
+        // Add buttonPanel to mainPanel
+        gbcPanels.gridx = 1;
+        gbcPanels.weightx = 0.1; // 10% of the main panel width
+        mainPanel.add(buttonPanel, gbcPanels);
 
-      mainPanel.add(viewerPanel, BorderLayout.CENTER);
-      mainPanel.add(buttonPanel, BorderLayout.EAST);
-
-      JButton openImageButton = new JButton("Open Image");
-      JButton trainImageButton = new JButton("Train Image");
-      JButton compressImageButton = new JButton("Compress Image");
-      JButton decompressImageButton = new JButton("Decompress Image");
-
-      openImageButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-      trainImageButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-      compressImageButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-      decompressImageButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-
-      /* 
-        // Add action listeners for the buttons
-      openImageButton.addActionListener(e -> openImage());
-      trainImageButton.addActionListener(e -> trainImage());
-      compressImageButton.addActionListener(e -> compressImage());
-      decompressImageButton.addActionListener(e -> decompressImage());
-      */
-
-      // Add buttons to the panel
-      buttonPanel.add(Box.createVerticalStrut(10));
-      buttonPanel.add(openImageButton);
-      buttonPanel.add(Box.createVerticalStrut(10)); // Add space between buttons
-      buttonPanel.add(trainImageButton);
-      buttonPanel.add(Box.createVerticalStrut(10));
-      buttonPanel.add(compressImageButton);
-      buttonPanel.add(Box.createVerticalStrut(10));
-      buttonPanel.add(decompressImageButton);
-      buttonPanel.add(Box.createVerticalGlue()); // Push buttons upwards if extra space
-
-      // Add panel to the frame
-      frame.add(mainPanel);
-
-      // Set the frame to be visible
-      frame.setVisible(true);
+        mainFrame.add(mainPanel, BorderLayout.CENTER);
+        mainFrame.setVisible(true);
     }
 
-    /* 
-    private void openImage() {
-        Operations.openImage();
+    public final void createMainFrame(){
+        mainFrame = new JFrame("Huffman Coding Image Compression");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(1600, 900);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setResizable(false);
+        mainFrame.setLayout(new BorderLayout());
     }
 
-    private void trainImage() {
-        Operations.trainImage();
+    public final void createMainPanel(){
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridBagLayout());
     }
 
-    private void compressImage() {
-        Operations.compressImage();
+    public final void createImagePanel(){
+        imagePanel = new JPanel();
+        imagePanel.setPreferredSize(new Dimension(500, 500));
+        imagePanel.setBackground(new Color(40, 40, 40));
+        imagePanel.setBorder(BorderFactory.createLineBorder(new Color(77, 77, 77), 10));
+        imagePanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        imagePanel.add(imageLabel, gbc); 
     }
 
-    private void decompressImage() {
-        Operations.decompressImage();
+    public final void createButtonPanel(){
+        buttonPanel = new JPanel();
+        buttonPanel.setBackground(new Color(77, 77, 77));
+        buttonPanel.setLayout(new GridBagLayout());
+        buttonContainerPanel = new JPanel();
+        buttonContainerPanel.setLayout(new GridLayout(4, 1, 10,30));
+        buttonContainerPanel.setOpaque(false);
+
+        designButtons();
+        
+        buttonContainerPanel.add(newBtn);
+        buttonContainerPanel.add(trainBtn);
+        buttonContainerPanel.add(compressBtn);
+        buttonContainerPanel.add(openBtn);
+
+        GridBagConstraints gbcButtons = new GridBagConstraints();
+        gbcButtons.gridx = 0;
+        gbcButtons.gridy = 0;
+        gbcButtons.anchor = GridBagConstraints.CENTER;
+        buttonPanel.add(buttonContainerPanel, gbcButtons);
     }
-    */
-  }
+
+    public final void designButtons(){
+
+        Dimension buttonSize = new Dimension(200, 70);
+        newBtn.setPreferredSize(new Dimension(buttonSize));
+        trainBtn.setPreferredSize(new Dimension(buttonSize));
+        compressBtn.setPreferredSize(new Dimension(buttonSize));
+        openBtn.setPreferredSize(new Dimension(buttonSize));
+
+        Color buttonColor = new  Color(200, 200, 200);
+        newBtn.setBackground(buttonColor);
+        trainBtn.setBackground(buttonColor);
+        compressBtn.setBackground(buttonColor);
+        openBtn.setBackground(buttonColor);
+
+        Color buttonBorderColor = new  Color(150, 150, 150);
+        newBtn.setBorder( BorderFactory.createLineBorder(buttonBorderColor, 6));
+        trainBtn.setBorder( BorderFactory.createLineBorder(buttonBorderColor, 6));
+        compressBtn.setBorder( BorderFactory.createLineBorder(buttonBorderColor, 6));
+        openBtn.setBorder( BorderFactory.createLineBorder(buttonBorderColor, 6));
+    }
+}

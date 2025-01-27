a9f76116454d589a27f52c8b8ed6bcfe536ccc64
Jade Eric Petilla <93184120+Schneidelstrom@users.noreply.github.com>
2025-01-27 21:10:39 +0800

    Huffman-Integration

---
+++ HuffmanNode.java
@@ -0,0 +1,17 @@
+package huffmancoding;
+
+public class HuffmanNode implements Comparable<HuffmanNode>{
+    public int frequency;
+    public int pixel;
+    public HuffmanNode left, right;
+
+    public HuffmanNode(int frequency, int pixel){
+        this.frequency = frequency;
+        this.pixel = pixel;
+        left = right = null;
+    }
+
+    public int compareTo(HuffmanNode other){
+        return frequency - other.frequency;
+    }
+}
